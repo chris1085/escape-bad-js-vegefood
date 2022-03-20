20804717f6a869445409876b73313273340bc850
Chris Chiang <itscfchiang@gmail.com>
2022-03-20 12:27:17 +0800

    fix: fix axios, function return problem and remove unused variavle(index)

--- js/all.js
+++ js/all.js
@@ -1,3 +1,4 @@
+/* global axios */
 // TODO: 修正 ESLint 錯誤、補上分號、前輩說要改單引號 QQ
 var url = 'https://hexschool.github.io/js-filter-data/data.json';
 var data;
@@ -5,7 +6,7 @@ var data;
 axios.get(url).then(function (res) {
   data = res.data.filter((a) => a.作物名稱);
   // TODO: 之後拆成 renderData 函式
-  table.innerHTML = renderData(data);
+  renderData(data);
 });
 
 var table = document.querySelector('.table-content');
@@ -18,13 +19,13 @@ filter.addEventListener('click', filterCategory);
 
 function renderData(data) {
   var str = '';
-  data.forEach((b, index) => {
+  data.forEach((b) => {
     // TODO: 改成 ES6 的 Template Literals (字面字串符)
     var content = `<tr><td>${b.作物名稱}</td><td>${b.市場名稱}</td><td>${b.上價}</td><td>${b.中價}</td><td>${b.下價}</td><td>${b.平均價}</td><td>${b.交易量}</td></tr>`;
     str += content;
   });
 
-  return str;
+  table.innerHTML = str;
 }
 
 function filterCategory(e) {
@@ -35,7 +36,7 @@ function filterCategory(e) {
     });
 
     // TODO: 之後拆成 renderData 函式
-    table.innerHTML = renderData(showData);
+    renderData(showData);
   } else {
     return;
   }
